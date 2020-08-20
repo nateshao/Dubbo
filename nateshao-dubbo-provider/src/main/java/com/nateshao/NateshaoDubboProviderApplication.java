5347bfe4b8e46fcd8fad0e5a585e21d46de28954
nateshao <shaotongjie@gmail.com>
2020-08-20 23:15:25 +0800

    项目初始化完成

---
+++ nateshao-dubbo-provider/src/main/java/com/nateshao/NateshaoDubboProviderApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoDubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoDubboProviderApplication.class, args);
+    }
+
+}
